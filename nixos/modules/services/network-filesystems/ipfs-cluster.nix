2c739b4bf5db511c4f73baada2c6e434650c08ff
Nathan Fish <lordcirth@gmail.com>
2019-10-19 19:20:30 +0000

    ipfs-cluster: basic service config

---
+++ nixos/modules/services/network-filesystems/ipfs-cluster.nix
@@ -0,0 +1,95 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  inherit (pkgs) ipfs ipfs-cluster runCommand makeWrapper;
+
+  cfg = config.services.ipfs-cluster;
+
+  ipfsClusterFlags = toString ([]);
+
+  defaultDataDir = "/var/lib/ipfs-cluster";
+
+  # Wrapping the ipfs-cluster-service binary with the environment variable IPFS_CLUSTER_PATH set to dataDir because we can't set it in the user environment
+  wrapped = runCommand "ipfs-cluster-service" { buildInputs = [ makeWrapper ]; preferLocalBuild = true; } ''
+    mkdir -p "$out/bin"
+    makeWrapper "${ipfs-cluster}/bin/ipfs-cluster-service" "$out/bin/ipfs-cluster-service" \
+      --set IPFS_CLUSTER_PATH ${cfg.dataDir} \
+      --prefix PATH : /run/wrappers/bin
+  '';
+
+  commonEnv = {
+    serviceConfig.User = cfg.user;
+    serviceConfig.Group = cfg.group;
+  };
+
+  baseService = recursiveUpdate commonEnv {
+    serviceConfig = {
+      ExecStart = "${wrapped}/bin/ipfs-cluster-service daemon ${ipfsClusterFlags}";
+      Restart = "on-failure";
+      RestartSec = 1;
+    };
+  };
+
+in {
+
+  ###### interface
+
+  options = {
+
+    services.ipfs-cluster = {
+
+      enable = mkEnableOption "IPFS pinset orchestrator (Requires an ipfs daemon to function)";
+
+      dataDir = mkOption {
+        type = types.str;
+        default = defaultDataDir;
+        description = "The data dir for IPFS Cluster";
+      };
+
+      user = mkOption {
+        type = types.str;
+        default = "ipfs-cluster";
+        description = "User under which the IPFS daemon runs";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "ipfs-cluster";
+        description = "Group under which the IPFS Cluster daemon runs";
+      };
+    };
+  };
+
+  ###### implementation
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ wrapped ];
+
+    users.users = mkIf (cfg.user == "ipfs-cluster") {
+      ipfs-cluster = {
+        group = cfg.group;
+        home = cfg.dataDir;
+        createHome = false;
+        uid = config.ids.uids.ipfs-cluster;
+        description = "IPFS daemon user";
+      };
+    };
+
+    users.groups = mkIf (cfg.group == "ipfs-cluster") {
+      ipfs-cluster.gid = config.ids.gids.ipfs-cluster;
+    };
+
+    systemd.tmpfiles.rules = [
+      "d '${cfg.dataDir}' - ${cfg.user} ${cfg.group} - -"
+    ];
+
+    systemd.services.ipfs-cluster-init = recursiveUpdate commonEnv {
+    };
+
+    systemd.services.ipfs-cluster = recursiveUpdate baseService {
+      description = "IPFS Cluster Daemon";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" "ipfs-cluster-init.service" ];
+      # conflicts = [ "ipfs-offline.service" "ipfs-norouting.service"];
+    };
+  };
+}
