d085c459b0802374034f4114669572c13ba4931c
Nathan <lordcirth@gmail.com>
2019-11-14 22:38:04 -0500

    ckan: first try at version 1.26.4
    Got Nuget and Cake in place

--- pkgs/games/ckan/default.nix
+++ pkgs/games/ckan/default.nix
@@ -1,28 +1,51 @@
-{ stdenv, fetchFromGitHub, makeWrapper, perl, mono, gtk2, curl }:
+{ stdenv, fetchFromGitHub, fetchNuGet, fetchurl, makeWrapper, perl, mono, gtk2, curl, unzip }:
 
+let
+  nugetVersion = "5.0.2";
+  # From packages.config in source
+  cakeVersion  = "0.34.1";
+in
 stdenv.mkDerivation rec {
   pname = "ckan";
-  version = "1.16.1";
+  version = "1.26.4";
 
   src = fetchFromGitHub {
     owner = "KSP-CKAN";
     repo = "CKAN";
     rev = "v${version}";
-    sha256 = "0lfvl8w09lakz35szp5grfvhq8xx486f5igvj1m6azsql4n929lg";
+    sha256 = "0ir6dc4z40mpbzk4zih5vf0i9ibn9k0qxy03jdvf80j6zyfwd8z5";
   };
 
-  buildInputs = [ makeWrapper perl mono ];
+  nuget = fetchurl {
+    url = "https://dist.nuget.org/win-x86-commandline/v${nugetVersion}/nuget.exe";
+    sha256 = "16svcf7z04ylmrxz1ayaxmdpbsghs58xgkpzwz1rh4a46vkych9y";
+  };
 
-  postPatch = ''
-    substituteInPlace bin/build \
-      --replace /usr/bin/perl ${perl}/bin/perl
-  '';
+  # fetchNuGet doesn't keep dll's?
+  cake = fetchurl {
+    url = "https://www.nuget.org/api/v2/package/Cake/${cakeVersion}";
+    sha256 = "0v3j72h1gw81ys6lak6bqxk7wn2ik4x2jd3kr2krr1pdpyp7b2pp";
+  };
+
+  buildInputs = [ makeWrapper perl mono unzip ];
 
   # Tests don't currently work, as they try to write into /var/empty.
   doCheck = false;
   checkTarget = "test";
 
   libraries = stdenv.lib.makeLibraryPath [ gtk2 curl ];
+
+  buildPhase = ''
+    libDir="_build/lib/nuget"
+    nugetDir="_build/tools/NuGet/${nugetVersion}"
+
+    mkdir -p $libDir $nugetDir
+
+    cp ${nuget} $nugetDir/nuget.exe
+    unzip ${cake} -d $libDir/Cake.${cakeVersion}
+
+    ./build
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
